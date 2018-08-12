3c2ced8e42e86a3d4193b2b55722625e2a0b7785
Yang Luo <hsluoyz@qq.com>
2018-08-13 00:07:38 +0800

    Improve the users.

--- website/pages/en/users.js
+++ website/pages/en/users.js
@@ -31,13 +31,13 @@ class Users extends React.Component {
         <Container padding={['bottom', 'top']}>
           <div className="showcaseSection">
             <div className="prose">
-              <h1>Who's Using This?</h1>
-              <p>This project is used by many folks</p>
+              <h1>Who's using Casbin?</h1>
+              <p>Hundreds of projects are using Casbin, from established Fortune 500 companies to hot new startups. If you&#x27;re curious to see what can be accomplished Casbin, check out these apps!</p>
             </div>
             <div className="logos">{showcase}</div>
-            <p>Are you using this project?</p>
-            <a href={editUrl} className="button">
-              Add your company
+            <p>Is your project also using Casbin?</p>
+            <a href="https://github.com/casbin/casbin-website/edit/master/website/siteConfig.js" className="button">
+              Add your company here
             </a>
           </div>
         </Container>
