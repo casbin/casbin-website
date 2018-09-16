1e1027eb51ba789bd853ffecf119f83b27f5c0a3
Yang Luo <hsluoyz@qq.com>
2018-09-17 00:49:21 +0800

    Remove blog link.

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -66,7 +66,7 @@ class Footer extends React.Component {
           </div>
           <div>
             <h5>More</h5>
-            <a href={this.props.config.baseUrl + 'blog'}>Blog</a>
+            <a href="https://github.com/casbin/casbin/wiki">Full Documentation</a>
             <a href={this.props.config.repoUrl}>GitHub</a>
             <a
               className="github-button"
