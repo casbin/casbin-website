9b7d3feae15f1d42011ceb17235b9c1bc945d3c5
Yang Luo <hsluoyz@qq.com>
2020-01-30 14:20:23 +0800

    Import casbin editor from GitHub pages.

--- website/pages/en/editor.js
+++ website/pages/en/editor.js
@@ -3,7 +3,7 @@ const React = require('react');
 const Editor = () => (
     <div>
       <iframe
-          src="https://optimistic-wozniak-4e6a8d.netlify.com"
+          src="https://casbin.org/casbin-editor/"
           style={{width: '100%', height: '100vh', display: 'block'}}
           title="Casbin-editor"
           allowFullScreen={true}
