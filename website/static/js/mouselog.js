e7caf476a0c710a08674b32f0ff5d6454716e87d
Yang Luo <hsluoyz@qq.com>
2020-02-15 16:00:52 +0800

    Update Mouselog script.

--- website/static/js/mouselog.js
+++ website/static/js/mouselog.js
@@ -1,8 +1,15 @@
 (function() {
   var script = document.createElement("script");
-  script.src = "https://cdn.jsdelivr.net/npm/mouselog@0.0.5-beta2/mouselog.js";
+  script.src = "https://cdn.jsdelivr.net/npm/mouselog@latest/build/mouselog.min.js";
   script.onload = () => {
-    mouselog.run("https://mouselog.org", "casbin");
+    let config = {
+      uploadEndpoint: "https://mouselog.org/",
+      websiteId: "casbin",
+      uploadMode: "periodic",
+      uploadPeriod: 5000
+    };
+    var agent = new mouselog.Mouselog();
+    agent.run(config);
   };
   var t = document.getElementsByTagName("script");
   var s = t.length > 0 ? t[0].parentNode : document.body;
