70265e8b857a8a564b59a7799a3e37cabbbd4976
Yang Luo <hsluoyz@qq.com>
2020-03-28 13:46:47 +0800

    Update mouselog script to 0.2.7

--- website/static/js/mouselog.js
+++ website/static/js/mouselog.js
@@ -1,20 +1,12 @@
 (function() {
   var script = document.createElement("script");
-  script.src = "https://cdn.jsdelivr.net/npm/mouselog@0.2.6/build/mouselog.min.js";
+  script.src = "https://cdn.jsdelivr.net/npm/mouselog@0.2.7/build/mouselog.min.js";
   script.onload = function() {
     var config = {
-      websiteId: "casbin_0.2.6",
+      websiteId: "casbin_0.2.7",
       uploadEndpoint: "https://mouselog.org/api/upload-trace",
-      uploadMode: "mixed",
-      uploadTimes: 1,
-      uploadPeriod: 2000,
-      frequency: 50,
-      sizeLimit: 7000,
-      enableGet: true,
-      encoder: "base64",
-      enableServerConfig: false,
-      enableSession: false,
-      enableSendEmpty: true
+      uploadMode: "periodic",
+      uploadPeriod: 5000
     };
     var agent = mouselog.init();
     agent.run(config);
