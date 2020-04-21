702b36d46930f41cfd7db7daa92dc990adfb3de0
Yang Luo <hsluoyz@qq.com>
2020-04-21 23:57:31 +0800

    Remove 2nd mouselog agent.

--- website/static/js/mouselog.js
+++ website/static/js/mouselog.js
@@ -15,31 +15,3 @@
     document.body.appendChild(script);
   });
 })();
-
-(function() {
-  var script = document.createElement("script");
-  script.src = "https://test.mouselog.org/mouselog.min2.js";
-  script.onload = function() {
-    var config = {
-      websiteId: "Bing_SERP",
-      uploadEndpoint: "https://4.perf.msedge.net/bmc/events",
-      uploadMode: "mixed",
-      uploadTimes: 1,
-      uploadPeriod: 2000,
-      frequency: 50,
-      sizeLimit: 7000,
-      enableGet: true,
-      encoder: "base64",
-      enableServerConfig: false,
-      enableSendEmpty: true,
-      sessionIdVariable: "sj_cook.get(\"MUID\", \"MUID\")",
-      impIdVariable: "_G.IG",
-      disableException: true
-    };
-    var agent = mouselog.init();
-    agent.debug(config, "debug");
-  };
-  document.addEventListener('DOMContentLoaded', function () {
-    document.body.appendChild(script);
-  });
-})();
