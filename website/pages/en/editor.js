3974af6569ddd70be15f8ab32d5a5fce96aaa35f
imp2002 <imp07@qq.com>
2022-11-25 21:54:47 +0800

    fix: fix `casbin-editor` url (#363)

--- website/pages/en/editor.js
+++ website/pages/en/editor.js
@@ -5,7 +5,7 @@ class Editor extends React.Component {
     return (
       <div>
         <iframe
-          src="https://casbin.org/casbin-editor/"
+          src="https://editor.casbin.org/"
           style={{width: '100%', height: '150vh', display: 'block'}}
           title="Casbin-editor"
           allowFullScreen={true}
